096842e98bc43994211f87e7806d8f5feb582f32
laxmiulavi <ulavilaxmi2@gmail.com>
2026-07-02 14:41:54 +0530

    Add WiFi connection constants and button state

---
+++ code/WIFI_connect.ino
@@ -0,0 +1,7 @@
+#include <WiFi.h>
+const char* ssid = "Shidlyali_F_Floor_2.4GHz";
+const char* password = "Shidlyali*2025#";
+const char* ssid = "*******************";
+const char* password = "*****************";
+int btnGPIO = 0;
+int btnState = false;
